85d999ba915146d3de76310520bd91cc38e3b830
Jeremiah Stump <stumpy224@gmail.com>
2015-02-09 23:22:30 -0500

    Set active navbar link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,53 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).on("page:change", function() {
+  return init();
+});
+
+$(function() {
+  init();
+});
+
+function init() {
+	setActiveNavbarLink();
+}
+
+function setActiveNavbarLink() {
+  if (window.location.pathname == '/')
+    setActiveNavbarLinkToRoot();
+  else if (window.location.pathname == '/bracket')
+    setActiveNavbarToBracket();
+  else if (window.location.pathname == '/grid')
+    setActiveNavbarToGrid();
+  else if (window.location.pathname == '/faq')
+    setActiveNavbarToFaq();
+}
+
+function setActiveNavbarToRoot() {
+  makeAllNavLinksInactive();
+  $('#nav_link_to_results').addClass('active');
+}
+
+function setActiveNavbarToBracket() {
+  makeAllNavLinksInactive();
+  $('#nav_link_to_bracket').addClass('active');
+}
+
+function setActiveNavbarToGrid() {
+  makeAllNavLinksInactive();
+  $('#nav_link_to_grid').addClass('active');
+}
+
+function setActiveNavbarToFaq() {
+  makeAllNavLinksInactive();
+  $('#nav_link_to_faq').addClass('active');
+}
+
+function makeAllNavLinksInactive() {
+  $('#nav_link_to_results').removeClass('active');
+  $('#nav_link_to_bracket').removeClass('active');
+  $('#nav_link_to_grid').removeClass('active');
+  $('#nav_link_to_faq').removeClass('active'); 
+}
