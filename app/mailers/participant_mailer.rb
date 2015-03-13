50cc4a7788688b21380b63340dc75301fd090af8
Jeremiah Stump <stumpy224@gmail.com>
2015-03-12 20:15:12 -0400

    Only send winner email if participant has an email

--- app/mailers/participant_mailer.rb
+++ app/mailers/participant_mailer.rb
@@ -7,6 +7,6 @@ class ParticipantMailer < ApplicationMailer
     @participant_squares = ParticipantSquare.where(participant_id: @participant.id, year: $year)
     @results = Result.where(participant_id: @participant.id, year: $year)
     @payout = Payout.find_by(round: game.round, year: $year)
-    mail(to: @participant.email, subject: "You have a winning square from TheGridPool.com!")
+    mail(to: @participant.email, subject: "You have a winning square from TheGridPool.com!") if !@participant.email.blank?
   end
 end
