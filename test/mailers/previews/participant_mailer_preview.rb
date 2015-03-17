31fe30bf66c7bd6e2e1b8538f8a6521a5d09940c
Jeremiah Stump <stumpy224@gmail.com>
2015-03-16 21:26:21 -0400

    remove hard-coded width from congratulations table

--- test/mailers/previews/participant_mailer_preview.rb
+++ test/mailers/previews/participant_mailer_preview.rb
@@ -1,4 +1,5 @@
-# Preview all emails at http://localhost:3000/rails/mailers/participant_mailer
+# Preview emails - http://localhost:3000/rails/mailers/participant_mailer
+# Preview game winner email - http://localhost:3000/rails/mailers/participant_mailer/game_winner_email_preview
 class ParticipantMailerPreview < ActionMailer::Preview
   def game_winner_email_preview
     $year = "2014"
@@ -8,13 +9,13 @@ class ParticipantMailerPreview < ActionMailer::Preview
   def create_preview_game
     g = Game.new
     g.round = 3
-    g.square_winner_id = 19
-    g.team_on_top_seed = 8
+    g.square_winner_id = 18
+    g.team_on_top_seed = 3
     g.team_on_top_name_short = "Purdue"
     g.team_on_top_score = 87
     g.team_on_top_is_winner = true
-    g.team_on_bottom_seed = 9
-    g.team_on_bottom_name_short = "Northwestern"
+    g.team_on_bottom_seed = 14
+    g.team_on_bottom_name_short = "Indiana"
     g.team_on_bottom_score = 78
     g.team_on_bottom_is_winner = false
     return g
